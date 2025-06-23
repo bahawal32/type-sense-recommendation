fa15648799ea3a7d6bf79479963561abfb911035
bahawal30 <bahawal30@gmail.com>
2025-06-23 13:54:06 +0500

    fix: make default host 'localhost' from typesense

--- fastapi-app/typesense_client.py
+++ fastapi-app/typesense_client.py
@@ -3,7 +3,7 @@ import os
 import logging
 logger = logging.getLogger(__name__)
 
-TYPESENSE_HOST = os.getenv("TYPESENSE_HOST", "typesense")
+TYPESENSE_HOST = os.getenv("TYPESENSE_HOST", "localhost")
 TYPESENSE_PORT = os.getenv("TYPESENSE_PORT", "8108")
 TYPESENSE_API_KEY = os.getenv("TYPESENSE_API_KEY")
 
@@ -14,33 +14,30 @@ client = typesense.Client({
         "protocol": "http"
     }],
     "api_key": TYPESENSE_API_KEY,
-    "connection_timeout_seconds": 2
+    "connection_timeout_seconds": 500,
+
 })
 
 def init_schema():
     logger.info("Initializing Typesense schema...")
-    books_schema = {
-        'name': 'books',
-        'fields': [
-            {'name': 'title', 'type': 'string' },
-            {'name': 'authors', 'type': 'string[]', 'facet': True },
+    news_article_schema = {
+        "name": "news_articles",
+        "fields": [
+            { "name": "link", "type": "string" },
+            { "name": "headline", "type": "string" },
+            { "name": "category", "type": "string", "facet": True },
+            { "name": "short_description", "type": "string" },
+            { "name": "authors", "type": "string", "facet": True },
+            { "name": "date", "type": "string", "facet": True }
+        ]
+        }
 
-            {'name': 'publication_year', 'type': 'int32', 'facet': True },
-            {'name': 'ratings_count', 'type': 'int32' },
-            {'name': 'average_rating', 'type': 'float' }
-        ],
-        'default_sorting_field': 'ratings_count'
-    }
-
-    try:
-        logger.info("Creating collection 'books'...")
-        client.collections.create(books_schema)
-    except Exception:
-        logger.warning("Collection 'books' already exists or could not be created.")
-        pass
+    logger.info("Creating collection 'news_articles'...")
+    client.collections.create(news_article_schema)
+  
         
 
 def import_sample_data():
     logger.info("Importing sample data into Typesense...")
-    with open('books.jsonl') as jsonl_file:
-        client.collections['books'].documents.import_(jsonl_file.read().encode('utf-8'))
+    with open('News_Category_Dataset_v3.jsonl') as jsonl_file:
+        client.collections['news_articles'].documents.import_(jsonl_file.read().encode('utf-8'))
